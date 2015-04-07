5921c8b9588e8bbb169534033a9d4efa1d60af00
Archit Srivastava <arch.srivastava.2@gmail.com>
2015-04-07 15:09:45 +0530

    [BUGFIX] Allow users to access dashboard

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -2,10 +2,10 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
+    can :read, :all                   # allow everyone to read everything
+    can :dashboard                  # allow access to dashboard
     if user && user.admin?
-      can :read, :all                   # allow everyone to read everything
       can :access, :rails_admin       # only allow admin users to access Rails Admin
-      can :dashboard                  # allow access to dashboard
       can :manage, :all
       # can :crud, :all             # allow superadmins to do anything
     end
