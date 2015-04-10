a5478adf3f503a403c9a8a8bfb6f2ccf344cd627
Archit Srivastava <arch.srivastava.2@gmail.com>
2015-04-11 03:43:20 +0530

    [BUGFIX] Removed mongoid devise hack after mongoid 4.0.2 update for warden.authenticate login error

--- app/models/user.rb
+++ app/models/user.rb
@@ -67,19 +67,12 @@ class User
   # field :unlock_token,    type: String # Only if unlock strategy is :email or :both
   # field :locked_at,       type: Time
 
-  class << self
-    def serialize_from_session(key, salt)
-      record = to_adapter.get(key[0]["$oid"])
-      record if record && record.authenticatable_salt == salt
-    end
-  end
-
   def title
     self.email
   end
 
   def active_for_authentication?
-    super && !self.disabled
+    super && !self.disable?
   end
 
 end
